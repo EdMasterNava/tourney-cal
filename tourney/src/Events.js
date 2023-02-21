23752b94f99067db73cb39973fd4a08e9ec62644
EdMasterNava <eduardonava2ms@gmail.com>
2023-02-20 18:16:34 -0800

    Added useEffect to update display list
    Also cleaned up code for readability.

--- tourney/src/Events.js
+++ tourney/src/Events.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { jsonObject } from './jsonObject';
 import Months from './Months';
+import List from './List';
 import './Events.css';
 
 function Events(){
@@ -10,74 +11,39 @@ function Events(){
         displayEventList: jsonObject,
         selectedMonth: 12
     });
-    // useEffect(() => {
-    //     if(eventListData.selectedMonth === 12){
-    //         setEventListData(prev => {
-    //             return {...prev, displayEventList: prev.completeEventList}
-    //         });
-    //         console.log(`displayEvents: ${eventListData.displayEventList}`);
-    //     }else{
-    //         const newArr = [];
-    //         eventListData.completeEventList.forEach(obj => {
-    //             if(eventListData.selectedMonth === new Date(obj["event_start_date"]).getMonth()){
-    //                 newArr.push(obj);
-    //             }
-    //         });
-    //         setEventListData(prev => {
-    //             return {...prev, displayEventList: newArr}
-    //         });
-    //         console.log(`displayEvents: ${eventListData.displayEventList}`);
-    //     }
-    // }, [eventListData.selectedMonth]);
+    //update displayEventList when selectedMonth is updated
+    useEffect(() => {
+        if(eventListData.selectedMonth == 12){
+            setEventListData(prev => {
+                return {...prev, displayEventList: prev.completeEventList}
+            });
+        }else{
+            const newArr = [];
+            eventListData.completeEventList.forEach(obj => {
+                if(eventListData.selectedMonth == new Date(obj["event_start_date"]).getMonth()){
+                    newArr.push(obj);
+                }
+            });
+            setEventListData(prev => {
+                return {...prev, displayEventList: newArr}
+            });
+        }
+    }, [eventListData.selectedMonth]);
+    //update selectedMonth
     const handleMonth = (event) => {
-        console.log(`handleMonth! target.id: ${event.target.id}`);
         setEventListData(prev => {
             return {...prev, selectedMonth: event.target.id}
         });
     }
-    // const filterEventsByDate = 
-    const getCompleteList = () =>{
-        return eventListData.completeEventList;
-    }
-    //sort not working with array of events
-    // const sortEvents = () => {
-    //     handleDisplayEventList();
-    //     // const sorted = eventListData.displayEventList.sort((a, b) => {
-    //     //     return (new Date(a["event_start_date"]).getTime()) - (new Date(b["event_start_date"].getTime()));
-    //     // })
-    //     return eventListData.displayEventList;
-    // };
     return (
-        <React.Fragment>
-            
-        <div id='events-container'>
-            <div id='upcoming'>
-                <h1>Upcoming Events</h1>
+        <React.Fragment>  
+            <div id='events-container'>
+                <div id='upcoming'>
+                    <h1>Upcoming Events</h1>
+                </div>
+                <Months monthData={eventListData} handleMonth={handleMonth}/>
+                <List displayEventList={eventListData.displayEventList}/>
             </div>
-
-            <Months monthData={eventListData} handleMonth={handleMonth}/>
-            {eventListData.displayEventList.map(obj => {
-                    return(
-                        <button className="event-block" id={obj['id']}>
-                            <div id="event-name">
-                                <p id="event-date">
-                                    {obj["event_start_date"] === obj["event_end_date"] ? 
-                                        new Date(obj["event_start_date"]).toDateString() : 
-                                        new Date(obj["event_start_date"]).toDateString() + ' - ' + 
-                                        new Date(obj["event_end_date"]).toDateString()
-                                    }
-                                </p>
-                                <p id="title">
-                                    {obj["event_name"]}
-                                </p>
-                                <p id="venue">
-                                    {obj["venue"]}
-                                </p>
-                            </div>
-                        </button>
-                    )
-                })}
-        </div>
         </React.Fragment>
     )
 };
